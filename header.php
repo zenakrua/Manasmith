316e811dd39b616b2f72b577dea0561c213d2a5c
Zen Akrua <zenakrua@gmail.com>
2018-10-31 21:05:49 -0400

    Adding phpmyadmin.

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<link rel="stylesheet" type="text/css" href="css/normalize.css" media="all"/>
+	<link rel="stylesheet" type="text/css" href="css/main.css" media="screen"/>
+</head>
+
+<body>
+
+	<header>
+	
+		<button value="Adventurers">Adventurers</button>
+		
+		<button value="Mana Circles">Mana Circles</button>
+			
+		<button value="Weapons">Weapons</button>
+			
+		<button value="Wyrmprints">Wyrmprints</button>
+			
+		<button value="Dragons">Dragons</button>
+			
+	</header>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include 'header.php';
+?>
+
+<div id="collection">
+
+	test testest
+	
+</div>
+
+<?php
+include 'footer.php'
+?>
+

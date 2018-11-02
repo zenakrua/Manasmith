690b84d46925b87dba2ae391242f93b7de8d6128
Zen Akrua <zenakrua@gmail.com>
2018-11-01 20:04:33 -0400

    Updating index.

--- index.php
+++ index.php
@@ -4,14 +4,9 @@ include 'header.php';
 
 <div id="collection">
 
-<<<<<<< HEAD
-	test testest TEEEEEEEST
-
-
-	big fucking mood
-=======
 BIG FUCKING MOOD
->>>>>>> 45be61914751bf59a5702d0db9eaf3a70ed24480
+
+test
 	
 </div>
 

61f37b61ed4e6dee53b10596733f72a91e019db0
zenakrua <zenakrua@msn.com>
2018-11-05 14:21:03 -0500

    Update functions.js

---
+++ scripts/functions.js
@@ -0,0 +1,14 @@
+$.ajax({
+			contentType: 'application/json; charset=UTF-8',
+			dataType: 'json',
+			url: 'functions.php',
+			success: function(data) {
+				$.each(data, function(key,value) {
+					var $name = value.name;
+					$("content").append($name);
+				});
+			},
+			error: function(data, errorThrown){
+				alert('Request failed: '+errorThrown);
+			}
+		});

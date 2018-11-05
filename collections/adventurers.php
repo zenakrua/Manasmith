14554bc0a895a33bd2c671585b51a0471207b202
zenakrua <zenakrua@msn.com>
2018-11-05 16:31:42 -0500

    Update adventurers.php

--- collections/adventurers.php
+++ collections/adventurers.php
@@ -12,9 +12,10 @@ $(document).ready(function() {
 		dataType: 'json',
 		url: 'functions.php',
 		success: function(data) {
-			$.each(data, function(key,value) {
-				var $name = value.name;
-				$("content").append($name);
+			alert("Success.");
+//			$.each(data, function(key,value) {
+//				var $name = value.name;
+//				$("content").append($name);
 			});
 		},
 		error: function(data, errorThrown){

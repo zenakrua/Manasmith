1d18dba2f0967f7dfbce0da2a4166598a5129ac2
zenakrua <zenakrua@msn.com>
2018-11-05 12:59:10 -0500

    Update adventurers.php

--- collections/adventurers.php
+++ collections/adventurers.php
@@ -2,28 +2,6 @@
 include 'header.php';
 ?>
 
-<?php
-
-//$host = "mysql:unix_socket=/cloudsql/manasmith-221002:us-east1:manasmith";
-//$user = "silke";
-//$pass = "Y1j5]NZb)GfH";
-//$database = "collections";
-
-	try {
-
-		$dbconnect = new PDO("mysql:host=$host;dbname=$database", $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-		$dbconnect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-	}
-
-	catch(PDOException $e) {
-
-		echo "Connection failed: " . $e->getMessage();
-
-	};
-
-?>
-
 <div id="collection">
 
 MUH PEEPS

2e809c2c81029d5b5196960f4555ade549f41953
zenakrua <zenakrua@msn.com>
2018-11-05 14:41:58 -0500

    Update adventurers.php

--- collections/adventurers.php
+++ collections/adventurers.php
@@ -3,7 +3,28 @@ include 'header.php';
 ?>
 
 <?php
-	require_once 'dbconnect.php';
+
+//$host = getenv("MYSQL_DSN");
+//$user = getenv("MYSQL_USER");
+//$pass = getenv("MYSQL_PASSWORD");
+
+	try {
+
+		$dbconnect = new PDO("$host", $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+		$dbconnect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	}
+
+	catch(PDOException $e) {
+
+		echo "Connection failed: " . $e->getMessage();
+
+	};
+
+?>
+
+<?php
+//	require_once 'dbconnect.php';
 	$query = $dbconnect->prepare("select * from adventurers");
 	$query->execute();
 	$jsonData = $query->fetchAll(PDO::FETCH_GROUP|PDO::FETCH_ASSOC);

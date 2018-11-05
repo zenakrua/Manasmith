17331ee3ea58f9e60dbd9bcda3e010cba56ccb2f
zenakrua <zenakrua@msn.com>
2018-11-05 14:31:59 -0500

    Update functions.js

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,14 +1,18 @@
-$.ajax({
-			contentType: 'application/json; charset=UTF-8',
-			dataType: 'json',
-			url: 'functions.php',
-			success: function(data) {
-				$.each(data, function(key,value) {
-					var $name = value.name;
-					$("content").append($name);
-				});
-			},
-			error: function(data, errorThrown){
-				alert('Request failed: '+errorThrown);
-			}
-		});
+$(document).ready(function() {
+
+	$.ajax({
+		contentType: 'application/json; charset=UTF-8',
+		dataType: 'json',
+		url: 'functions.php',
+		success: function(data) {
+			$.each(data, function(key,value) {
+				var $name = value.name;
+				$("content").append($name);
+			});
+		},
+		error: function(data, errorThrown){
+			alert('Request failed: '+errorThrown);
+		}
+	});
+
+});

db6c8677abc8de3550a48659be900b1312ef306d
zenakrua <zenakrua@msn.com>
2018-11-05 14:15:04 -0500

    Update adventurers.php

--- collections/adventurers.php
+++ collections/adventurers.php
@@ -4,21 +4,6 @@ include 'header.php';
 
 <div id="collection">
 	
-<?php
-
-	require_once 'dbconnect.php';
-
-	$query = $dbconnect->prepare("select * from adventurers");
-	$query->execute();
-
-	$jsonData = $query->fetchAll(PDO::FETCH_GROUP|PDO::FETCH_ASSOC);
-
-	echo json_encode($jsonData);
-
-	$dbconnect = null;
-	
-?>
-	
 </div>
 
 <?php

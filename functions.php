e7bbec1f7eac0bddea66e3f835b64a12a08ffc36
zenakrua <zenakrua@msn.com>
2018-11-05 17:54:46 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
 	$query = $dbconnect->prepare("select name from adventurers");
 	$query->execute();
 
-	$jsonData = $query->fetchAll(PDO::FETCH_GROUP|PDO::FETCH_ASSOC);
+	$jsonData = $query->fetchAll(PDO::FETCH_ASSOC);
 
 	echo json_encode($jsonData);
 //	echo $jsonData;

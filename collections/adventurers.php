3ad053349c508747b5f485b67ce376afc0c5be50
zenakrua <zenakrua@msn.com>
2018-11-05 16:25:41 -0500

    Update adventurers.php

--- collections/adventurers.php
+++ collections/adventurers.php
@@ -3,6 +3,29 @@ include 'header.php';
 include 'functions.php';
 ?>
 
+<script>
+
+$(document).ready(function() {
+
+	$.ajax({
+		contentType: 'application/json; charset=UTF-8',
+		dataType: 'json',
+		url: 'functions.php',
+		success: function(data) {
+			$.each(data, function(key,value) {
+				var $name = value.name;
+				$("content").append($name);
+			});
+		},
+		error: function(data, errorThrown){
+			alert('Request failed: '+errorThrown);
+		}
+	});
+
+});
+	
+</script>
+
 <content>
 	
 test

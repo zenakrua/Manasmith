bcc52fc633b38f15318a2fa53223656b8857f000
zenakrua <zenakrua@msn.com>
2018-11-05 18:00:09 -0500

    Update adventurers.php

--- adventurers.php
+++ adventurers.php
@@ -2,30 +2,6 @@
 include 'header.php';
 ?>
 
-<script>
-
-$(document).ready(function() {
-
-	$.ajax({
-		contentType: 'application/json; charset=UTF-8',
-		dataType: 'json',
-		url: 'functions.php',
-		success: function(data) {
-//			alert('Success.');
-			$.each(data, function(key,value) {
-				var $name = value.name;
-				$("#content").append($name);
-			});
-		},
-		error: function(data, errorThrown){
-			alert('Request failed: '+errorThrown);
-		}
-	});
-
-});
-	
-</script>
-
 <div id="content">
 	
 test

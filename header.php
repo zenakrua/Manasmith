7e182ad3d8c1a125e2baf1f09e7d7b6bdc85c69a
zenakrua <zenakrua@msn.com>
2018-11-05 14:52:29 -0500

    Update header.php

--- header.php
+++ header.php
@@ -10,7 +10,7 @@ error_reporting(E_ALL);
 	<link rel="stylesheet" type="text/css" href="css/normalize.css" media="all"/>
 	<link rel="stylesheet" type="text/css" href="css/main.css" media="screen"/>
 	<script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-<!--	<script type="text/javascript" src="scripts/functions.js"></script> -->
+	<script type="text/javascript" src="scripts/functions.js"></script>
 </head>
 
 <body>

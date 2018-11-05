779ea4569142b68d198174d7b785a95fd07f74ea
zenakrua <zenakrua@msn.com>
2018-11-05 13:55:34 -0500

    Update and rename sqlconnect.php to dbconnect.php

---
+++ scripts/dbconnect.php
@@ -0,0 +1,20 @@
+<?php
+
+//$host = getenv("MYSQL_DSN");
+//$user = getenv("MYSQL_USER");
+//$pass = getenv("MYSQL_PASSWORD");
+
+	try {
+
+		$dbconnect = new PDO("$host", $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+		$dbconnect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	}
+
+	catch(PDOException $e) {
+
+		echo "Connection failed: " . $e->getMessage();
+
+	};
+
+?>

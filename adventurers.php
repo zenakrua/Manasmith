6fdcbed21d1cb00e9507e405222abdc35e961b84
zenakrua <zenakrua@msn.com>
2018-11-06 18:17:07 -0500

    Update adventurers.php

--- adventurers.php
+++ adventurers.php
@@ -1,28 +1,26 @@
 <?php
+
 include 'header.php';
 
 require_once 'dbconnect.php';
 	$query = $dbconnect->prepare("select * from adventurers");
 	$query->execute();
 	$jsonData = $query->fetchAll(PDO::FETCH_ASSOC);
-	$json = json_encode($jsonData);
+//	$json = json_encode($jsonData);
 //	echo $jsonData;
 	$dbconnect = null;
 
 echo '<div id="content">';
 
-foreach($json->data as $mydata)
+foreach($jsonData->data as $mydata)
 
     {
-         echo $mydata->name . "\n";
-         foreach($mydata->values as $values)
-         {
-              echo $values->value . "\n";
-         }
+         echo $mydata->name . "\n"
     }  
 
 echo '</div>';
 
 include 'footer.php'
+	
 ?>
 

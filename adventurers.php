c2fe61937c67a8e392026ec30e33836507d5b656
zenakrua <zenakrua@msn.com>
2018-11-06 13:56:24 -0500

    Update adventurers.php

--- adventurers.php
+++ adventurers.php
@@ -2,6 +2,23 @@
 include 'header.php';
 ?>
 
+<?php
+
+	require_once 'dbconnect.php';
+
+	$query = $dbconnect->prepare("select * from adventurers");
+	$query->execute();
+
+	$jsonData = $query->fetchAll(PDO::FETCH_ASSOC);
+
+	echo json_encode($jsonData);
+//	echo $jsonData;
+
+	$dbconnect = null;
+	
+?>
+
+
 <script>
 	
 $(document).ready(function() {

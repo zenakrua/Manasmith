925b29ed054ad01f77193cd9899492b3801a9703
zenakrua <zenakrua@msn.com>
2018-11-06 16:12:29 -0500

    Update adventurers.php

--- adventurers.php
+++ adventurers.php
@@ -12,10 +12,10 @@ $(document).ready(function() {
 		success: function(data) {
 //			alert('Success.');
 			$.each(data, function(key,value) {
-				$options = ['size' => 120, 'crop' => true];
-				$image_file = "gs://${manasmith-221002.appspot.com}/"+value.id+"_"+value.variation+".png";
+				$options = array('size' => 120, 'crop' => true);
+				$image_file = 'gs://${manasmith-221002.appspot.com}/'+value.id+'_'+value.variation+'.png';
 				$image_url = CloudStorageTools::getImageServingUrl($image_file, $options);
-				$("#content").append(value.name+"<img src="+$image_url);
+				$('#content').append(value.name+'<img src='+$image_url);
 			});
 		},
 		error: function(data, errorThrown){

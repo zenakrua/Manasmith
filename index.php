3807c151fa81609a9c0baa0a2fc7f8e465fa562a
zenakrua <zenakrua@msn.com>
2018-11-09 15:20:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
+
 include 'header.php';
+
+if ($_SERVER['REQUEST_URI']=='/') {
+	include 'index.php';
+} elseif ($_SERVER['REQUEST_URI']=='/adventurers') {
+	include 'adventurers.php';
+} elseif ($_SERVER['REQUEST_URI']=='/mana-circles') {
+	include 'mana-circles.php';
+} elseif ($_SERVER['REQUEST_URI']=='/weapons') {
+	include 'weapons.php';
+} elseif ($_SERVER['REQUEST_URI']=='/wyrmprints') {
+	include 'wyrmprints.php';
+} elseif ($_SERVER['REQUEST_URI']=='/dragons') {
+	include 'dragons.php';
+} else {
+	include '404.php';
+}
+	
 ?>
 
 <div id="collection">

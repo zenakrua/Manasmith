55bcaf3e202d544d0cf7f768978412dbb304b98b
zenakrua <zenakrua@msn.com>
2018-11-09 15:45:06 -0500

    Update adventurers.php

--- adventurers.php
+++ adventurers.php
@@ -8,6 +8,12 @@ require_once 'dbconnect.php';
 	echo $json;
 	$dbconnect = null;
 
+$options = ['size' => 400, 'crop' => true];
+$image_file = "gs://${manasmith-221002.appspot.com}/images/adventurers/portraits/110029_1.png";
+$image_url = CloudStorageTools::getImageServingUrl($image_file, $options);
+
+echo $image_url;
+
 //foreach($jsonData->data as $mydata)
 
 //    {

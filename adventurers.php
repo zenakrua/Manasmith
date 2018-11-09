2104d88911b82bb9e5220c753ebc79976b6a7878
zenakrua <zenakrua@msn.com>
2018-11-09 16:55:46 -0500

    Update adventurers.php

--- adventurers.php
+++ adventurers.php
@@ -1,12 +1,12 @@
 <?php
 
 require_once 'dbconnect.php';
-	$query = $dbconnect->prepare("select * from adventurers");
-	$query->execute();
-	$jsonData = $query->fetchAll(PDO::FETCH_ASSOC);
-	$adventurers = json_encode($jsonData);
-//	echo $json;
-	$dbconnect = null;
+$query = $dbconnect->prepare("select * from adventurers");
+$query->execute();
+$jsonData = $query->fetchAll(PDO::FETCH_ASSOC);
+$adventurers = json_encode($jsonData);
+//echo $json;
+$dbconnect = null;
 
 //$options = ['size' => 400, 'crop' => true];
 //$image_file = "gs://${manasmith-221002.appspot.com}/images/adventurers/portraits/110029_1.png";

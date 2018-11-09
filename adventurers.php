150068e997475a2106714d0845283d86729f11f3
zenakrua <zenakrua@msn.com>
2018-11-09 18:49:19 -0500

    Update adventurers.php

--- adventurers.php
+++ adventurers.php
@@ -1,29 +1,32 @@
 <?php
 
 require_once 'dbconnect.php';
-$query = $dbconnect->prepare("select * from adventurers");
-$query->execute();
-$adventurers = $query->fetchAll(PDO::FETCH_ASSOC);
-//$adventurers = json_encode($jsonData);
-//echo $json;
-$dbconnect = null;
+	$query = $dbconnect->prepare("select * from adventurers order by element asc, rarity desc, name asc, variation asc");
+	$query->execute();
+	$adventurers = $query->fetchAll(PDO::FETCH_ASSOC);
+//	$list = json_encode($jsonData);
+//	var_dump($jsonData);
+//	echo $list;
+	$dbconnect = null;
 
-//$options = ['size' => 400, 'crop' => true];
-//$image_file = "gs://${manasmith-221002.appspot.com}/images/adventurers/portraits/110029_1.png";
-//$image_url = CloudStorageTools::getImageServingUrl($image_file, $options);
+echo '<div id="content">';
 
-//echo $image_url;
+$options = ['size' => 400, 'crop' => true];
+$image_file = "gs://manasmith-221002.appspot.com/images/adventurers/portraits/110029_1.png";
+$image_url = CloudStorageTools::getImageServingUrl($image_file, $options);
 
-//var_dump($adventurers);
+echo $image_url;
 
-foreach($adventurers->data as $adventurer){
-	
-         foreach($adventurer->values as $values){
-		 
-              echo $values->name . "\n";
-		 
-         }
-	
-    }  
+foreach($adventurers as $adventurer){
+
+	$options = ['size' => 400, 'crop' => true];
+	$image_file = 'gs://${manasmith-221002.appspot.com}/'.$adventurer['id'].'_'.$adventurer['variation'].'.jpg';
+//	$image_url = CloudStorageTools::getImageServingUrl($image_file, $options);
+//	$image_url = 'img';
+
+//	echo '<table class="adventurer"><tr><th>'.$adventurer['name'].'</th></tr><tr><td><img src="'.$image_url.'"></td></tr></table>';
+	echo '<table class="adventurer"><tr><th>'.$adventurer['name'].'</th></tr><tr><td><img src="gs://${manasmith-221002.appspot.com}/images/adventurers/portraits/'.$adventurer['id'].'_'.$adventurer['variation'].'.png"></td></tr></table>';
+
+}
 	
 ?>

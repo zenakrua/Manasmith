21f8ef91a43b4eb2a32de5da22f73cbb3124d45c
Zen Akrua <zenakrua@gmail.com>
2018-11-09 23:55:41 -0500

    Trying to get gcloud tools working

--- adventurers.php
+++ adventurers.php
@@ -12,15 +12,17 @@ $dbconnect = null;
 foreach($adventurers as $adventurer){
 
 	$options = ['size' => 400, 'crop' => true];
-	$image_file = 'gs://manasmith-221002.appspot.com/'.$adventurer['id'].'_'.$adventurer['variation'].'.png';
-//	var_dump($image_file);
-	$image_url = CloudStorageTools::getImageServingUrl($image_file, $options);
-//	var_dump($image_url);
+//	$image_file = 'gs://manasmith-221002.appspot.com/'.$adventurer['id'].'_'.$adventurer['variation'].'.png';
+	$image_url = 'gs://manasmith-221002.appspot.com/'.$adventurer['id'].'_'.$adventurer['variation'].'.png';
+	//	var_dump($image_file);
+//	$image_url = CloudStorageTools::getImageServingUrl($image_file, $options);
+	var_dump($image_url);
+	echo $image_url;
 //	$image_url = 'img';
 
 //	echo $adventurer['name'];
 
-	echo '<table class="adventurer"><tr><th>'.$adventurer['name'].'</th></tr><tr><td>test</td></tr></table>';
+	echo '<table class="adventurer"><tr><th>'.$adventurer['name'].'</th></tr><tr><td>'.$image_url.'</td></tr></table>';
 //	echo '<table class="adventurer"><tr><th>'.$adventurer['name'].'</th></tr><tr><td><img src="gs://${manasmith-221002.appspot.com}/images/adventurers/portraits/'.$adventurer['id'].'_'.$adventurer['variation'].'.png"></td></tr></table>';
 
 }

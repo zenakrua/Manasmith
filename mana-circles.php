3bd7e6b1b7f29e5d75f1090fe2fec3959cec21ed
Zen Akrua <zenakrua@gmail.com>
2018-11-11 00:02:18 -0500

    Testing cloud functions

--- mana-circles.php
+++ mana-circles.php
@@ -1,1 +1,12 @@
+<?php
 
+require 'vendor/autoload.php';
+
+use Google\Cloud\Storage\StorageClient;
+
+$storage = new StorageClient();
+$storage->registerStreamWrapper();
+
+$contents = file_get_contents('gs://manasmith-221002.appspot.com/test.txt');
+
+?>

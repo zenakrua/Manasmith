3bc3bef1ced7d436f0cfc1bf2f263abbd9f90222
Zen Akrua <zenakrua@gmail.com>
2018-11-10 21:14:01 -0500

    Testing public link

--- adventurers.php
+++ adventurers.php
@@ -9,23 +9,23 @@ $adventurers = $query->fetchAll(PDO::FETCH_ASSOC);
 //echo $list;
 $dbconnect = null;
 
-include_once APPPATH . '/vendor/autoload.php';
-use google\appengine\api\cloud_storage\CloudStorageTools;
+//include_once APPPATH . '/vendor/autoload.php';
+//use google\appengine\api\cloud_storage\CloudStorageTools;
 
 foreach($adventurers as $adventurer){
 
-	$options = ['size' => 400, 'crop' => true];
-	$image_file = 'gs://manasmith-221002.appspot.com/'.$adventurer['id'].'_'.$adventurer['variation'].'.png';
-	$image_url = 'gs://manasmith-221002.appspot.com/images/adventurers/portraits/'.$adventurer['id'].'_'.$adventurer['variation'].'.png';
+//	$options = ['size' => 400, 'crop' => true];
+//	$image_file = 'gs://manasmith-221002.appspot.com/'.$adventurer['id'].'_'.$adventurer['variation'].'.png';
+//	$image_url = 'gs://manasmith-221002.appspot.com/images/adventurers/portraits/'.$adventurer['id'].'_'.$adventurer['variation'].'.png';
 //	var_dump($image_file);
-	$image_url = CloudStorageTools::getImageServingUrl($image_file, $options);
+//	$image_url = CloudStorageTools::getImageServingUrl($image_file, $options);
 //	var_dump($image_url);
 //	echo $image_url;
 //	$image_url = 'img';
 
 //	echo $adventurer['name'];
 
-	echo '<table class="adventurer"><tr><th>'.$adventurer['name'].'</th></tr><tr><td><img src="'.$image_url.'" /></td></tr></table>';
+	echo '<table class="adventurer"><tr><th>'.$adventurer['name'].'</th></tr><tr><td><img src="http://storage.googleapis.com/manasmith-221002.appspot.com/images/adventurers/portraits/'.$adventurer['id'].'_'.$adventurer['variation'].'.png" /></td></tr></table>';
 //	echo '<table class="adventurer"><tr><th>'.$adventurer['name'].'</th></tr><tr><td><img src="gs://${manasmith-221002.appspot.com}/images/adventurers/portraits/'.$adventurer['id'].'_'.$adventurer['variation'].'.png"></td></tr></table>';
 
 }

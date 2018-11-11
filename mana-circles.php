ad10f38f06bc45d9a13b1af22e03b1e6549f4040
Zen Akrua <zenakrua@gmail.com>
2018-11-11 00:28:52 -0500

    Test

--- mana-circles.php
+++ mana-circles.php
@@ -6,7 +6,6 @@ use Google\Cloud\Storage\StorageClient;
 
 $storage = new StorageClient();
 $storage->registerStreamWrapper();
-var_dump($storage);
 
 $contents = file_get_contents('gs://manasmith-221002.appspot.com/test.txt');
 

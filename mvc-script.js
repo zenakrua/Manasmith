2c414a2709ec9993a6cea8fc4e6b724499b93deb
zenakrua <zenakrua@msn.com>
2019-02-28 15:12:56 -0500

    Update mvc-script.js

--- mvc-script.js
+++ mvc-script.js
@@ -1,41 +1,95 @@
-function main(){
-  var model = new Model();
-  var controller = new Controller(model);
-  var view = new View(controller);
-}
+var PenguinModel = function PenguinModel(XMLHttpRequest) {
+  this.XMLHttpRequest = XMLHttpRequest;
+};
 
-function View(controller){
-    this.controller = controller;
-    this.heading = document.getElementById('heading');
-    this.heading.innerText = controller.getModelHeading();
-    this.heading.addEventListener('click', controller);
-}
+PenguinModel.prototype.getPenguin = function getPenguin(index, fn) {
+  var oReq = new this.XMLHttpRequest();
 
-function Model(){
-    this.heading = "Hello";
-}
+  oReq.onload = function onLoad(e) {
+    var ajaxResponse = JSON.parse(e.currentTarget.responseText);
+    var penguin = ajaxResponse[index];
 
-function Controller(model){
-  var self = this;
-  this.model = model;
-//EVENTLISTENER INTERFACE
-  this.handleEvent = function(e){
-    e.stopPropagation();
-    switch(e.type){
-      case "click":
-        self.clickHandler(e.target);
-        break;
-      default:
-        console.log(e.target);
-    }
+    penguin.index = index;
+    penguin.count = ajaxResponse.length;
+
+    fn(penguin);
+  };
+
+  oReq.open('GET', 'https://codepen.io/beautifulcoder/pen/vmOOLr.js', true);
+  oReq.send();
+};
+
+var PenguinView = function PenguinView(element) {
+  this.element = element;
+
+  this.onClickGetPenguin = null;
+};
+
+PenguinView.prototype.render = function render(viewModel) {
+  this.element.innerHTML = '<h3>' + viewModel.name + '</h3>' +
+    '<img class="penguin-image" src="' + viewModel.imageUrl +
+      '" alt="' + viewModel.name + '" />' +
+    '<p><b>Size:</b> ' + viewModel.size + '</p>' +
+    '<p><b>Favorite food:</b> ' + viewModel.favoriteFood + '</p>' +
+    '<a id="previousPenguin" class="previous button" href="javascript:void(0);"' +
+      ' data-penguin-index="' + viewModel.previousIndex + '">Previous</a> ' +
+    '<a id="nextPenguin" class="next button" href="javascript:void(0);"' +
+      ' data-penguin-index="' + viewModel.nextIndex + '">Next</a>';
+
+  this.previousIndex = viewModel.previousIndex;
+  this.nextIndex = viewModel.nextIndex;
+
+  var previousPenguin = this.element.querySelector('#previousPenguin');
+  previousPenguin.addEventListener('click', this.onClickGetPenguin);
+
+  var nextPenguin = this.element.querySelector('#nextPenguin');
+  nextPenguin.addEventListener('click', this.onClickGetPenguin);
+};
+
+var PenguinController = function PenguinController(penguinView, penguinModel) {
+  this.penguinView = penguinView;
+  this.penguinModel = penguinModel;
+};
+
+PenguinController.prototype.initialize = function initialize() {
+  this.penguinView.onClickGetPenguin = this.onClickGetPenguin.bind(this);
+};
+
+PenguinController.prototype.onClickGetPenguin = function onClickGetPenguin(e) {
+  var target = e.currentTarget;
+  var index = parseInt(target.dataset.penguinIndex, 10);
+
+  this.penguinModel.getPenguin(index, this.showPenguin.bind(this));
+};
+
+PenguinController.prototype.showPenguin = function showPenguin(penguinModelData) {
+  var penguinViewModel = {
+    name: penguinModelData.name,
+    imageUrl: penguinModelData.imageUrl,
+    size: penguinModelData.size,
+    favoriteFood: penguinModelData.favoriteFood
+  };
+
+  penguinViewModel.previousIndex = penguinModelData.index - 1;
+  penguinViewModel.nextIndex = penguinModelData.index + 1;
+
+  if (penguinModelData.index === 0) {
+    penguinViewModel.previousIndex = penguinModelData.count - 1;
   }
-//GET MODEL HEADING
-  this.getModelHeading = function(){
-    return self.model.heading;
+
+  if (penguinModelData.index === penguinModelData.count - 1) {
+    penguinViewModel.nextIndex = 0;
   }
-//CHANGE THE MODEL
-  this.clickHandler = function(target){
-    self.model.heading = 'World';
-    target.innerText = self.getModelHeading();
-  }
-}
+ this.penguinView.render(penguinViewModel);
+};
+
+var penguinModel = new PenguinModel(XMLHttpRequest);
+
+var targetElement = document.getElementById('listOfPenguins');
+var penguinView = new PenguinView(targetElement);
+
+var controller = new PenguinController(penguinView, penguinModel);
+
+controller.initialize();
+
+controller.onClickGetPenguin({ currentTarget: { dataset: { penguinIndex: 0 } } });

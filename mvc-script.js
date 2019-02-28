433826ffdbbc06ffbc11acdefb9d3485b7f162d0
zenakrua <zenakrua@msn.com>
2019-02-28 13:13:06 -0500

    Update mvc-script.js

--- mvc-script.js
+++ mvc-script.js
@@ -1,6 +1,6 @@
 function View(controller){
     this.controller = controller;
-    this.heading = document.getElementById(‘heading’);
+    this.heading = document.getElementById('heading');
     this.heading.innerText = controller.getModelHeading();
     this.heading.addEventListener('click', controller);
 }

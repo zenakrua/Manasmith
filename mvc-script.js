201bc2236cfb097513b2e4bc8ea7d45affde533b
zenakrua <zenakrua@msn.com>
2019-03-01 16:15:33 -0500

    Update mvc-script.js

--- mvc-script.js
+++ mvc-script.js
@@ -1,17 +1,40 @@
-var AdventurerController = function AdventurerController(adventurerView, adventurerModel) {
+function fetchContent(url) {
+	var xhttp;
+	xhttp = new XMLHttpRequest();
+	xhttp.responseType = "text";
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("content").insertAdjacentHTML("beforeend",xhttp.responseText);
+		}
+	};
+	xhttp.open("GET", url, true);
+	xhttp.send();
+	return;
+}
 
-	this.adventurerView = adventurerView;
-	this.adventurerModel = adventurerModel;
+function includeContent() {
 	
-};
+	switch (location.pathname) {
+		case "/":
+			fetchContent('home.html');
+			break;
+		case "/adventurers/":
+			fetchContent('adventurers.html');
+			break;
+		case "/mana-circles/":
+			fetchContent('mana-circles.html');
+			break;
+		case "/weapons/":
+			fetchContent('weapons.html');
+			break;
+		case "/wyrmprints/":
+			fetchContent('wyrmprints.html');
+			break;
+		case "/dragons/":
+			fetchContent('dragons.html');
+			break;
+		default:
+			fetchContent('404.html');
+	}
 
-AdventurerController.prototype.initialize = function initialize() {
-  this.adventurerView.onClickGetAdventurer = this.onClickGetAdventurer.bind(this);
-};
-
-AdventurerController.prototype.onClickGetAdventurer = function onClickGetAdventurer(e) {
-  var target = e.currentTarget;
-  var index = parseInt(target.dataset.adventurerIndex, 10);
-
-  this.adventurerModel.getPenguin(index, this.showPenguin.bind(this));
-};
+}

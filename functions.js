4f603e54f78aa8aa2213a0f8978e722dbb71e338
Sarah Schaller <zenakrua@gmail.com>
2019-03-01 23:53:31 -0500

    A lot of testing

--- functions.js
+++ functions.js
@@ -1,53 +1,42 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-	
-	function fetchData(url) {
-		var xhttp;
-		xhttp = new XMLHttpRequest();
-		xhttp.responseType = "json";
-		xhttp.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				document.getElementsByTagName("content")[0].insertAdjacentHTML("beforeend",xhttp.responseText);
-			}
-		};
-		xhttp.open("GET", url, true);
-		xhttp.send();
-		return;
-	};
+document.addEventListener("DOMContentLoaded", function (event) {
 
-	function fetchContent(url) {
+	function fetchContent() {
 		var xhttp;
+		var content = document.getElementsByTagName("content")[0];
+
+		switch (location.pathname) {
+			case "/":
+				url = "home.html";
+				break;
+			case "/adventurers/":
+				url = "adventurers.html";
+				break;
+			case "/mana-circles/":
+				url = "mana-circles.html";
+				break;
+			case "/weapons/":
+				url = "weapons.html";
+				break;
+			case "/wyrmprints/":
+				url = "wyrmprints.html";
+				break;
+			case "/dragons/":
+				url = "dragons.html";
+				break;
+			default:
+				url = "404.html";
+		}
+
 		xhttp = new XMLHttpRequest();
 		xhttp.responseType = "text";
-		xhttp.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				document.getElementsByTagName("content")[0].insertAdjacentHTML("beforeend",xhttp.responseText);
+		xhttp.onreadystatechange = function () {
+			if (xhttp.readyState === 4 && xhttp.status === 200) {
+				document.getElementsByTagName("content")[0].insertAdjacentHTML("beforeend", xhttp.responseText);
 			}
 		};
 		xhttp.open("GET", url, true);
 		xhttp.send();
 		return;
-	};
-	
-	switch (location.pathname) {
-		case "/manasmith/":
-			fetchContent('home.html');
-			break;
-		case "/manasmith/adventurers/":
-			fetchContent('adventurers.html');
-			break;
-		case "/manasmith/mana-circles/":
-			fetchContent('mana-circles.html');
-			break;
-		case "/manasmith/weapons/":
-			fetchContent('weapons.html');
-			break;
-		case "/manasmith/wyrmprints/":
-			fetchContent('wyrmprints.html');
-			break;
-		case "/manasmith/dragons/":
-			fetchContent('dragons.html');
-			break;
-		default:
-			fetchContent('404.html');
-	};
+	}
+
 });

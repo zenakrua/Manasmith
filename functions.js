b097b67bbd695673b020c0a464b0e7dde0af39c4
zenakrua <zenakrua@msn.com>
2019-04-11 12:49:26 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,34 +1,21 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-	
-	function fetchContent(section,content) {
-		
+	var nav = document.getElementsByClassName("nav");
+	for (i = 0; i < nav.length; i++) {
+		nav[i].addEventListener("click", function() {
+			var val = this.value;
+			fetchContent(val)
+		});
+	}
+	function fetchContent(val) {
 		var xhttp;
 		xhttp = new XMLHttpRequest();
 		xhttp.responseType = "text";
 		xhttp.onreadystatechange = function () {
-			
 			if (xhttp.readyState === 4 && xhttp.status === 200) {
-				
-				document.getElementsByTagName(section)[0].innerHTML = xhttp.responseText;
-				
+				document.getElementsByTagName("content")[0].innerHTML = xhttp.responseText;
 			}
 		};
 		xhttp.open("GET", val, true);
 		xhttp.send();
 	}
-	
-	fetchContent("content","home.html");
-	
-	var nav = document.getElementsByClassName("nav");
-	for (i = 0; i < nav.length; i++) {
-		
-		nav[i].addEventListener("click", function() {
-			
-			var val = this.value;
-			fetchContent(val)
-			
-		});
-		
-	}
-	
 });

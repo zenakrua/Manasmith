7ae3c8001bae429371fa0d4144e45a6444c6c8f5
zenakrua <zenakrua@msn.com>
2019-04-18 16:32:23 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -5,13 +5,7 @@ function fetchContent(section,content) {
 	xhttp.onreadystatechange = function () {
 		if (xhttp.readyState === 4 && xhttp.status === 200) {
 			var rawData = xhttp.response;
-			switch(content){
-			  case "data/adventurers.json":
-				fetchAdventurers(section,rawData);
-				break;
-			  default:
-				document.getElementsByTagName(section)[0].innerHTML = "No data.";
-			}
+			fetchData(rawData);
 		} else if (xhttp.readyState === 4 && xhttp.status === 404) {
 			document.getElementsByTagName(section)[0].innerHTML = "BOOM GOES THE WYRMITE";
 		}
@@ -20,7 +14,47 @@ function fetchContent(section,content) {
 	xhttp.send();
 }
 
+function fetchData(rawData) {
+	switch(content){
+		case "data/adventurers.json":
+			fetchAdventurers(section,rawData);
+			break;
+		case "data/weapons.json":
+			fetchWeapons(section,rawData);
+		break;
+		case "data/wyrmprints.json":
+			fetchWyrmprints(section,rawData);
+		break;
+		case "data/dragons.json":
+			fetchDragons(section,rawData);
+		break;
+		default:
+			document.getElementsByTagName(section)[0].innerHTML = "No data.";
+	}
+}
+
 function fetchAdventurers(section,rawData) {
+		var data = "";
+		for (i = 0; i < rawData.length; i++) {
+			document.getElementsByTagName(section)[0].innerHTML = data += rawData[i].Name;
+		}
+}
+
+function fetchWeapons(section,rawData) {
+		var data = "";
+		for (i = 0; i < rawData.length; i++) {
+			document.getElementsByTagName(section)[0].innerHTML = data += rawData[i].Name;
+		}
+}
+
+function fetchWyrmprints(section,rawData) {
+		var data = "";
+		for (i = 0; i < rawData.length; i++) {
+			document.getElementsByTagName(section)[0].innerHTML = data += rawData[i].Name;
+		}
+}
+
+function fetchDragons(section,rawData) {
 		var data = "";
 		for (i = 0; i < rawData.length; i++) {
 			document.getElementsByTagName(section)[0].innerHTML = data += rawData[i].Name;

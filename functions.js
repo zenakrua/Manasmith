ebdb8cc8822881af24ea04f32867d85726e0817c
zenakrua <zenakrua@msn.com>
2019-04-18 17:34:46 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -37,8 +37,8 @@ function fetchAdventurers(section,rawData) {
 		var data = "";
 		for (i = 0; i < rawData.length; i++) {
 			document.getElementsByTagName(section)[0].innerHTML = data
-				+= "<table class=\"adventurer" +  rawData[i].Element  + rawData[i].Rarity + "collected0\">"
-					+ "<tr class=\"header\">"
+				+= "<table class=\"adventurer\"" + " " + rawData[i].Element + " " + rawData[i].Rarity + " " + "\"collected0\">"
+					+ "<tr class='header'>"
 						+ "<th>" + rawData[i].Name + "</th>"
 					+ "</tr>"
 					+ "<tr class=\"icon\">"

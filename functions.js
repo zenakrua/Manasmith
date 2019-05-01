2aad6cf7b08d4778677f553144e0207b6b922016
zenakrua <zenakrua@msn.com>
2019-05-01 11:46:08 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -27,48 +27,57 @@ document.addEventListener("DOMContentLoaded", function (event) {
 });
 
 function fetchAdventurers() {
-		var data = "";
-		adventurers = adventurers.sort(function (a, b) {
-			return a.ElementID - b.ElementID || a.WeaponID - b.WeaponID || b.Rarity - a.Rarity || b.ID - a.ID || a.Name - b.Name;
-		});
-		for (i = 0; i < adventurers.length; i++) {
-			document.getElementsByTagName("content")[0].innerHTML = data
-				+= "<p class=\"adventurer" + " " + adventurers[i].Element + " " + adventurers[i].Rarity + " " + "collected" + adventurers[i].Collected + "\" onclick=\"fetchManaCircles(" + adventurers[i].ID + ");\">"
-					+ adventurers[i].Name
-				+ "</p>";
-		}
+	var data = "";
+	adventurers = adventurers.sort(function (a, b) {
+		return a.ElementID - b.ElementID || a.WeaponID - b.WeaponID || b.Rarity - a.Rarity || b.ID - a.ID || a.Name - b.Name;
+	});
+	for (i = 0; i < adventurers.length; i++) {
+		document.getElementsByTagName("content")[0].innerHTML = data
+			+= "<p class=\"adventurer" + " " + adventurers[i].Element + " " + adventurers[i].Rarity + " " + "collected" + adventurers[i].Collected + "\" onclick=\"fetchManaCircles(" + adventurers[i].ID + ");\">"
+				+ adventurers[i].Name
+			+ "</p>";
+	}
 }
 
 function fetchManaCircles(id) {
-		var data = "";
-		for (i = 0; i < adventurers.length; i++) {
-			if (adventurers[i].ID == id) {
+	var data = "";
+	for (i = 0; i < adventurers.length; i++) {
+		if (adventurers[i].ID == id) {
+			var circles = adventurers[i].Circles;
+			for (i = 0; i < circles.length; i++) {
 				var subcontent = document.getElementsByTagName("subcontent")[0];
 				if (subcontent.hasChildNodes()) {
 					subcontent.removeChild(subcontent.childNodes[0]);
 				}
-				subcontent.innerHTML = "<div>" + id + "</div>";
+				subcontent.innerHTML = "<table class=\"circle\" id=\"circle" + circles[i].Circle + "\">Circle " + circles[i].Circle + "</table>";
+				var nodes = circles[i].Nodes;
+				var circle = document.getElementById("circle" + circles[i].Circle);
+				var node = "";
+				for (i = 0; nodes.length; i++) {
+					circle.innerHTML = node += "<tr><th class=\"node\" id=\"node" + nodes[i].Node + "\">" + nodes[i].Node + "</th></tr>";
+				}
 			}
 		}
+	}
 }
 
 function fetchWeapons() {
-		var data = "";
-		for (i = 0; i < rawData.length; i++) {
-			document.getElementsByTagName("content")[0].innerHTML = data += rawData[i].Name;
-		}
+	var data = "";
+	for (i = 0; i < rawData.length; i++) {
+		document.getElementsByTagName("content")[0].innerHTML = data += rawData[i].Name;
+	}
 }
 
 function fetchWyrmprints() {
-		var data = "";
-		for (i = 0; i < rawData.length; i++) {
-			document.getElementsByTagName("content")[0].innerHTML = data += rawData[i].Name;
-		}
+	var data = "";
+	for (i = 0; i < rawData.length; i++) {
+		document.getElementsByTagName("content")[0].innerHTML = data += rawData[i].Name;
+	}
 }
 
 function fetchDragons() {
-		var data = "";
-		for (i = 0; i < rawData.length; i++) {
-			document.getElementsByTagName("content")[0].innerHTML = data += rawData[i].Name;
-		}
+	var data = "";
+	for (i = 0; i < rawData.length; i++) {
+		document.getElementsByTagName("content")[0].innerHTML = data += rawData[i].Name;
+	}
 }

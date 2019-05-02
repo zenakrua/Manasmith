362d8f3d1a15d56471fc3ef1c5dbb47541a634ac
Sarah Schaller <zenakrua@gmail.com>
2019-05-01 23:25:27 -0400

    Reverting functions to semi-working state

--- functions.js
+++ functions.js
@@ -5,19 +5,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
 	for (i = 0; i < nav.length; i++) {
 		nav[i].addEventListener("click", function () {
 			var content = this.value;
-			switch(content){
+			switch (content) {
 				case "adventurers":
 					fetchAdventurers("content");
 					break;
 				case "weapons":
 					fetchWeapons("content");
-				break;
+					break;
 				case "wyrmprints":
 					fetchWyrmprints("content");
-				break;
+					break;
 				case "dragons":
 					fetchDragons("content");
-				break;
+					break;
 				default:
 					document.getElementsByTagName("content")[0].innerHTML = "No data.";
 			}
@@ -31,23 +31,33 @@ function fetchAdventurers() {
 	adventurers = adventurers.sort(function (a, b) {
 		return a.ElementID - b.ElementID || a.WeaponID - b.WeaponID || b.Rarity - a.Rarity || b.ID - a.ID || a.Name - b.Name;
 	});
-	for (i in adventurers) {
+	for (i = 0; i < adventurers.length; i++) {
 		document.getElementsByTagName("content")[0].innerHTML = data
 			+= "<p class=\"adventurer" + " " + adventurers[i].Element + " " + adventurers[i].Rarity + " " + "collected" + adventurers[i].Collected + "\" onclick=\"fetchManaCircles(" + adventurers[i].ID + ");\">"
-				+ adventurers[i].Name
+			+ adventurers[i].Name
 			+ "</p>";
 	}
 }
 
 function fetchManaCircles(id) {
 	var data = "";
-	for (i in adventurers) {
+	for (i = 0; i < adventurers.length; i++) {
 		if (adventurers[i].ID == id) {
 			var subcontent = document.getElementsByTagName("subcontent")[0];
-			if (subcontent.hasChildNodes()) {
-				subcontent.removeChild(subcontent.childNodes[0]);
+			var circles = adventurers[i].Circles;
+			var circleList = "";
+			for (i = 0; i < circles.length; i++) {
+				if (subcontent.hasChildNodes()) {
+					subcontent.removeChild(subcontent.childNodes[0]);
+				}
+				subcontent.innerHTML = circleList += "<table class=\"circle\" id=\"circle" + circles[i].Circle + "\">Circle " + circles[i].Circle + "</table>";
+				var circle = document.getElementById("circle" + circles[i].Circle);
+				var nodes = circles[i].Nodes;
+				var nodeList = "";
+				for (i = 0; nodes.length; i++) {
+					circle.innerHTML = nodeList += "<tr><th class=\"node\" id=\"node" + nodes[i].Node + "\">" + nodes[i].Node + "</th></tr>";
+				}
 			}
-			subcontent.innerHTML = circleList += "<table class=\"circle\" id=\"circle" + circles[i].Circle + "\">Circle " + circles[i].Circle + "</table>";
 		} else {
 			var subcontent = document.getElementsByTagName("subcontent")[0];
 			if (subcontent.hasChildNodes()) {

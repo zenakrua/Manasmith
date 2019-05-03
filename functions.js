71eb3c3158254d0b635258a2289a9a3274f50b8a
zenakrua <zenakrua@msn.com>
2019-05-03 07:05:40 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,17 +1,11 @@
-// content elements
-var nav = document.getElementsByClassName("nav");
-var content = document.getElementsByTagName("content")[0];
-var subcontent = document.getElementsByTagName("subcontent")[0];
-	
-function empty(target) {
-	while (target.hasChildNodes()) {
-		target.removeChild(target.firstChild);
-	}
-}
-
-// When a navigation button is clicked, get json data based on which button and dump it into the content html element.
 document.addEventListener("DOMContentLoaded", function (event) {
 
+	// content elements
+	nav = document.getElementsByClassName("nav");
+	content = document.getElementsByTagName("content")[0];
+	subcontent = document.getElementsByTagName("subcontent")[0];
+
+	// When a navigation button is clicked, get json data based on which button and dump it into the content html element.
 	for (i = 0; i < nav.length; i++) {
 		nav[i].addEventListener("click", function () {
 			var data = this.value;
@@ -36,48 +30,59 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 });
 
+function empty(target) {
+	while (target.hasChildNodes()) {
+		target.removeChild(target.firstChild);
+	}
+}
+
 function fetchAdventurers() {
-	var adventurerList = "";
+	var adventurerData = "";
 	adventurers = adventurers.sort(function (a, b) {
 		return a.ElementID - b.ElementID || a.WeaponID - b.WeaponID || b.Rarity - a.Rarity || b.ID - a.ID || a.Name - b.Name;
 	});
-	for (i = 0; i < adventurers.length; i++) {
+	for (i in adventurers) {
 		var name = adventurers[i].Name;
 		var element = adventurers[i].Element;
 		var rarity = adventurers[i].Rarity;
 		var collected = adventurers[i].Collected;
 		var id = adventurers[i].ID;
-		content.innerHTML = adventurerList
-			+= "<p class=\"adventurer" + " " + element + " " + rarity + " " + "collected" + collected + "\" onclick=\"fetchManaCircles(" + id + ");\">"
+		content.innerHTML = adventurerData
+			+= "<button value=\"" + id + "\" class=\"adventurer" + " " + element + " " + rarity + " " + "collected" + collected + "\">"
 			+ name
-			+ "</p>";
+		+ "</button><br>";
+	}
+
+	var adventurer = document.getElementsByClassName("adventurer");
+	for (i = 0; i < adventurer.length; i++) {
+		adventurer[i].addEventListener("click", function () {
+			//subcontent.innerHTML = adventurers.find(fetchManaCircles);
+			console.log(adventurers.find(fetchManaCircles));
+		});
 	}
 }
 
-function fetchManaCircles(id) {
-	var data = "";
-	empty(subcontent);
-	for (i = 0; i < adventurers.length; i++) {
-		if (adventurers[i].ID == id) {
-			var circles = adventurers[i].Circles;
-			var circleList = "";
-			for (i = 0; i < circles.length; i++) {
-				subcontent.innerHTML = circleList += "<table class=\"circle\" id=\"circle" + circles[i].Circle + "\">Circle " + circles[i].Circle + "</table>";
-			}
-		} else {
-			subcontent.innerHTML = adventurers[i].Name + "'s mana circles haven't been revealed yet.";
-		}
-	}
+function fetchManaCircles(adventurerID) {
+
+	return adventurerID = adventurers.ID;
+
 }
 
 function fetchWeapons() {
+	empty(content);
+	empty(subcontent);
+	content.insertAdjacentHTML("beforeend", "Weapons");
 	return;
 }
 
 function fetchWyrmprints() {
+	empty(content);
+	content.insertAdjacentHTML("beforeend", "Wyrmprints");
 	return;
 }
 
 function fetchDragons() {
+	empty(content);
+	content.insertAdjacentHTML("beforeend", "Dragons");
 	return;
 }

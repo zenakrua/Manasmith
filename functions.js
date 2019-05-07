bd6ee22cf4cbd899527e76a16b9d550b2f936367
zenakrua <zenakrua@msn.com>
2019-05-07 09:47:06 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,40 +1,39 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 	
-fetch('data/checklist.json')
-	.then(function(response) {
-	return response.json();
-})
-.then(function(myJson) {
-	console.log(JSON.stringify(myJson));
-});
+	fetch('data/checklist.json')
+		.then(function(response) {
+		return response.json();
+	})
+	.then(function(myJson) {
+		console.log(JSON.stringify(myJson));
+		// content elements
+		nav = document.getElementsByClassName("nav");
+		content = document.getElementsByTagName("content")[0];
+		subcontent = document.getElementsByTagName("subcontent")[0];
 
-	// content elements
-	nav = document.getElementsByClassName("nav");
-	content = document.getElementsByTagName("content")[0];
-	subcontent = document.getElementsByTagName("subcontent")[0];
-
-	// When a navigation button is clicked, get json data based on which button and dump it into the content html element.
-	for (i = 0; i < nav.length; i++) {
-		nav[i].addEventListener("click", function () {
-			var data = this.value;
-			switch (data) {
-				case "adventurers":
-					fetchAdventurers();
-					break;
-				case "weapons":
-					fetchWeapons();
-					break;
-				case "wyrmprints":
-					fetchWyrmprints();
-					break;
-				case "dragons":
-					fetchDragons();
-					break;
-				default:
-					content.innerHTML = "No data.";
-			}
-		});
-	}
+		// When a navigation button is clicked, get json data based on which button and dump it into the content html element.
+		for (i = 0; i < nav.length; i++) {
+			nav[i].addEventListener("click", function () {
+				var data = this.value;
+				switch (data) {
+					case "adventurers":
+						fetchAdventurers();
+						break;
+					case "weapons":
+						fetchWeapons();
+						break;
+					case "wyrmprints":
+						fetchWyrmprints();
+						break;
+					case "dragons":
+						fetchDragons();
+						break;
+					default:
+						content.innerHTML = "No data.";
+				}
+			});
+		}
+	});
 
 });
 
@@ -49,33 +48,13 @@ function fetchAdventurer(adventurers) {
 	}
 
 function fetchAdventurers() {
-	var adventurerData = "";
-	adventurers = adventurers.sort(function (a, b) {
-		return a.ElementID - b.ElementID || a.WeaponID - b.WeaponID || b.Rarity - a.Rarity || b.ID - a.ID || a.Name - b.Name;
+	fetch('data/adventurers.json')
+		.then(function(response) {
+		return response.json();
+	})
+	.then(function(myJson) {
+		console.log(JSON.stringify(myJson));
 	});
-	for (i in adventurers) {
-		var name = adventurers[i].Name;
-		var element = adventurers[i].Element;
-		var rarity = adventurers[i].Rarity;
-		var collected = adventurers[i].Collected;
-		var id = adventurers[i].ID;
-		content.innerHTML = adventurerData
-			+= "<button value=\"" + id + "\" class=\"adventurer " + element + " " + rarity + " collected" + collected + "\">"
-			+ name
-			+ "</button><br>";
-	}
-	
-	var adventurer = document.getElementsByClassName("adventurer");
-	for (i = 0; i < adventurer.length; i++) {
-		adventurer[i].addEventListener("click", function () {
-			//map button value to adventurerID variable
-			adventurerID = this.value;
-			//remap adventurer variable as adventurer object
-			adventurer = (adventurers.find(fetchAdventurer, adventurerID));
-			//print adventurer name
-			subcontent.innerHTML = "<h1>" + adventurer.Name + "</h1>";			
-		});
-	}
 }
 
 //print list of adventurer's mana circles 

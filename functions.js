9169c8f351c13190d8a779993a05915d23aac367
Sarah Schaller <zenakrua@gmail.com>
2019-05-10 19:18:04 -0400

    UNFUCKING SHIT UP

--- functions.js
+++ functions.js
@@ -1,108 +1,157 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-	
-	fetch('data/checklist.json')
-		.then(function(response) {
-		console.log(response);
-		return response.json();
-	})
-	.then(function(checklist) {
-		// content elements
-		nav = document.getElementsByClassName("nav");
-		content = document.getElementsByTagName("content")[0];
-		subcontent = document.getElementsByTagName("subcontent")[0];
-
-		// When a navigation button is clicked, get json data based on which button and dump it into the content html element.
-		for (i = 0; i < nav.length; i++) {
-			nav[i].addEventListener("click", function () {
-				var data = this.value;
-				switch (data) {
-					case "adventurers":
-						fetchAdventurers(checklist);
-						break;
-					case "weapons":
-						fetchWeapons(checklist);
-						break;
-					case "wyrmprints":
-						fetchWyrmprints(checklist);
-						break;
-					case "dragons":
-						fetchDragons(checklist);
-						break;
-					default:
-						content.innerHTML = "No data.";
-				}
-			});
-		}
-	});
-
-});
+// content elements
+var nav = document.getElementsByClassName("nav")
+var content = document.getElementsByTagName("content")[0]
+var subcontent = document.getElementsByTagName("subcontent")[0]
 
 function empty(target) {
 	while (target.hasChildNodes()) {
-		target.removeChild(target.firstChild);
+		target.removeChild(target.firstChild)
 	}
 }
 
-function fetchAdventurer(adventurers) {
-		return adventurers.ID === adventurerID;
-	}
-
-function fetchAdventurers() {
-	fetch('data/adventurers.json')
-		.then(function(response) {
-		return response.json();
+fetch("data/checklist.json")
+	.then(function (response) {
+		return response.json()
 	})
-	.then(function(adventurers) {
-		var adventurerData = "";
-			adventurers = adventurers.sort(function (a, b) {
-				return a.ElementID - b.ElementID || a.WeaponID - b.WeaponID || b.Rarity - a.Rarity || b.ID - a.ID || a.Name - b.Name;
-			});
-			for (i in adventurers) {
-				var name = adventurers[i].Name;
-				var element = adventurers[i].Element;
-				var rarity = adventurers[i].Rarity;
-				var collected = adventurers[i].Collected;
-				var id = adventurers[i].ID;
-				content.innerHTML = adventurerData
-					+= "<button value=\"" + id + "\" class=\"adventurer " + element + " " + rarity + " collected" + collected + "\">"
-					+ name
-					+ "</button><br>";
-			}
-
-			function fetchAdventurer(adventurers) {
-				return adventurers.ID === adventurerID;
-			}
-
-			var adventurer = document.getElementsByClassName("adventurer");
-			for (i = 0; i < adventurer.length; i++) {
-				adventurer[i].addEventListener("click", function () {
-					//map button value to adventurerID variable
-					adventurerID = this.value;
-					//remap adventurer variable as adventurer object
-					adventurer = (adventurers.find(fetchAdventurer, adventurerID));
-					//print adventurer name
-					subcontent.innerHTML = "<h1>" + adventurer.Name + "</h1>";			
-				});
-			}
+	.then(function (json) {
+		var checklist
+		for (var i = 0; i < nav.length; i++) {
+			nav[i].addEventListener("click", function () {
+				var data = this.value
+				switch (data) {
+					case "adventurers":
+						checklist = json[0].adventurers
+						fetchAdventurers(checklist)
+						break
+					case "weapons":
+						checklist = json[0].weapons
+						fetchWeapons(checklist)
+						break
+					case "wyrmprints":
+						checklist = json[0].wyrmprints
+						fetchWyrmprints(checklist)
+						break
+					case "dragons":
+						checklist = json[0].dragons
+						fetchDragons(checklist)
+						break
+					default:
+						content.innerHTML = "No data."
+				}
+			})
 		}
-	});
+	})
+	.catch(function (error) {
+		console.log("Error.")
+	})
+
+var x
+var list
+var id
+function fetchAdventurers(checklist) {
+	empty(content)
+	empty(subcontent)
+
+	var adventurer
+	fetch("data/adventurers.json")
+		.then(function (response) {
+			return response.json()
+		})
+		.then(function (json) {
+			list = json
+			list = list.sort(function (a, b) {
+				return (
+					a.ElementID - b.ElementID ||
+					a.WeaponID - b.WeaponID ||
+					b.Rarity - a.Rarity ||
+					b.ID - a.ID ||
+					a.Name - b.Name
+				)
+			})
+			for (x in list) {
+				content.innerHTML +=
+					'<button class="adventurer" value="' +
+					list[x].ID +
+					'">' +
+					list[x].Name +
+					"</button><br>"
+			}
+			var adventurerNames = content.getElementsByTagName("button")
+			for (x in adventurerNames) {
+				adventurerNames[x].addEventListener("click", function () {
+					id = this.value
+					adventurer = list.find(findID, id)
+					subcontent.innerHTML = "<h1>" + adventurer.Name + "</h1>"
+					fetchCircles(adventurer)
+				})
+			}
+			return
+		})
 }
 
-function fetchWeapons() {
-	empty(content);
-	empty(subcontent);
-	content.insertAdjacentHTML("beforeend", "Weapons");
-	return;
+var circle
+function fetchCircles(adventurer) {
+	for (x in adventurer.Circles) {
+		circle = adventurer.Circles[x]
+		subcontent.insertAdjacentHTML(
+			"beforeend",
+			"<table class='circle' id='circle" +
+			circle.Circle +
+			"'><h2>Circle " +
+			circle.Circle +
+			"</h2></table>"
+		)
+		fetchNodes(circle)
+	}
 }
 
-function fetchWyrmprints() {
-	empty(content);
-	content.insertAdjacentHTML("beforeend", "Wyrmprints");
-	return;
+var node
+function fetchNodes(circle) {
+	for (x in circle.Nodes) {
+		node = circle.Nodes[x]
+		var nodeList = document.getElementById("circle" + circle.Circle)
+		nodeList.insertAdjacentHTML(
+			"beforeend",
+			"<tr><th colspan='2' class='node' id='node" +
+			node.Node +
+			"'>" +
+			node.Reward +
+			"</th></tr>"
+		)
+		for (x in node.Materials) {
+			nodeList.insertAdjacentHTML(
+				"beforeend",
+				"<tr><td>" +
+				node.Materials[x].Material +
+				"</td><td>" +
+				node.Materials[x].Amount +
+				"</td></tr>"
+			)
+		}
+	}
 }
 
-function fetchDragons() {
-	empty(content);
-	content.insertAdjacentHTML("beforeend", "Dragons");
-	return;
+function findID(list) {
+	return list.ID === id
+}
+
+function fetchWeapons(checklist) {
+	empty(content)
+	empty(subcontent)
+	subcontent.innerHTML = "Weapons"
+	return
+}
+
+function fetchWyrmprints(checklist) {
+	empty(content)
+	empty(subcontent)
+	subcontent.innerHTML = "Wyrmprints"
+	return
+}
+
+function fetchDragons(checklist) {
+	empty(content)
+	empty(subcontent)
+	subcontent.innerHTML = "Dragons"
+	return
 }

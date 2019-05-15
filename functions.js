2068721e2defb10bfc0e55fef534bd7a6f11e277
Sarah Schaller <zenakrua@gmail.com>
2019-05-15 18:43:58 -0400

    Updating json data

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+// document.addEventListener("DOMContentLoaded", function(event) {
 // content elements
 var nav = document.getElementsByClassName("nav")
 var content = document.getElementsByTagName("content")[0]
@@ -176,4 +176,4 @@ function fetchDragons(checklist) {
 	subcontent.innerHTML = "Dragons"
 	return
 }
-});
+// });
